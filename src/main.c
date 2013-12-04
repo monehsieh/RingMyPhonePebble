d256bbf6829385dd915d2ead9dbde85758f2c379
Siu Man Hsieh <monehsieh@gmail.com>
2013-12-04 01:05:54 -0800

    updated code for Pebble SDK 2 Beta2.

--- src/main.c
+++ src/main.c
@@ -1,30 +1,20 @@
 
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
-
-#define MY_UUID { 0xD1, 0xAD, 0x93, 0x6B, 0x17, 0x68, 0x46, 0x0E, 0x88, 0x5A, 0xD7, 0x96, 0x89, 0x6A, 0x38, 0xC2 }
-PBL_APP_INFO(MY_UUID, "Ring My Phone", "Dark Rock Studios", 1 , 0/* App version */, RESOURCE_ID_IMAGE_MENU_ICON, APP_INFO_STANDARD_APP);
 
 const int STATUS_RESET_TIME = 5000;
-const int STATUS_COOKIE = 1;
 
-Window window;
+static Window *window = NULL;
+static TextLayer *statusTextLayer = NULL;
+static ActionBarLayer *actionBar = NULL;
 
-TextLayer statusTextLayer;
+static GBitmap* bitmapRing = NULL;
+static GBitmap* bitmapSilence = NULL;
 
-AppContextRef appContext;
-ActionBarLayer actionBar;
-HeapBitmap bitmapRing;
-HeapBitmap bitmapSilence;
-
-AppTimerHandle statusResetTimer = APP_TIMER_INVALID_HANDLE;
+static AppTimer* statusResetTimer = NULL;
 
 static bool callbacks_registered = false;
-static AppMessageCallbacksNode app_callbacks;
 
-const char * const WINDOW_TITLE = "Ring My Phone";
 const char * const STATUS_READY = "Ready";
 const char * const STATUS_DONE = "Done";
 const char * const STATUS_RINGING = "Ringing";
@@ -32,12 +22,12 @@ const char * const STATUS_SILENCING = "Silencing";
 const char * const STATUS_FAILED = "Failed :(";
 
 enum {
-  CMD_KEY = 0x0, // TUPLE_INTEGER
+    CMD_KEY = 0x0, // TUPLE_INTEGER
 };
 
 enum {
-  CMD_START = 0x01,
-  CMD_STOP = 0x02,
+    CMD_START = 0x01,
+    CMD_STOP = 0x02,
 };
 
 static void send_cmd(uint8_t cmd);
@@ -45,188 +35,202 @@ void start_reset_timer();
 
 // Modify these common button handlers
 
-void up_single_click_handler(ClickRecognizerRef recognizer, Window *window)
+void up_single_click_handler(ClickRecognizerRef recognizer, void *context)
 {
-  (void)recognizer;
-  (void)window;
-
-	text_layer_set_text(&statusTextLayer, STATUS_RINGING);
-	send_cmd( CMD_START );
+    text_layer_set_text(statusTextLayer, STATUS_RINGING);
+    send_cmd( CMD_START );
 }
 
 
-void down_single_click_handler(ClickRecognizerRef recognizer, Window *window)
+void down_single_click_handler(ClickRecognizerRef recognizer, void *context)
 {
-  (void)recognizer;
-  (void)window;
-
-	text_layer_set_text(&statusTextLayer, STATUS_SILENCING);	
-	send_cmd( CMD_STOP );
+    text_layer_set_text(statusTextLayer, STATUS_SILENCING);
+    send_cmd( CMD_STOP );
 }
 
 // App message callbacks
-static void app_send_failed(DictionaryIterator* failed, AppMessageResult reason, void* context)
+static void app_send_failed(DictionaryIterator *iterator, AppMessageResult reason, void *context)
 {
-	text_layer_set_text(&statusTextLayer, STATUS_FAILED);
-	start_reset_timer();
+    text_layer_set_text(statusTextLayer, STATUS_FAILED);
+    start_reset_timer();
 }
 
 static void app_received_msg(DictionaryIterator* received, void* context)
 {
-	vibes_short_pulse();
-	text_layer_set_text(&statusTextLayer, STATUS_DONE);
-	start_reset_timer();
+    vibes_short_pulse();
+    text_layer_set_text(statusTextLayer, STATUS_DONE);
+    start_reset_timer();
 }
 
 bool register_callbacks()
 {
-	if (callbacks_registered) {
-		if (app_message_deregister_callbacks(&app_callbacks) == APP_MSG_OK)
-			callbacks_registered = false;
-	}
-	if (!callbacks_registered) {
-		app_callbacks = (AppMessageCallbacksNode){
-			.callbacks = {
-				.out_failed = app_send_failed,
-        .in_received = app_received_msg
-			},
-			.context = NULL
-		};
-		if (app_message_register_callbacks(&app_callbacks) == APP_MSG_OK) {
-      callbacks_registered = true;
+    if (callbacks_registered) {
+        app_message_deregister_callbacks();
+        callbacks_registered = false;
     }
-	}
-	return callbacks_registered;
+
+    if (!callbacks_registered)
+    {
+        app_message_register_outbox_failed( app_send_failed );
+        app_message_register_inbox_received( app_received_msg );
+        callbacks_registered = true;
+    }
+
+    return callbacks_registered;
 }
+
+void handle_timer(void *data)
+{
+    text_layer_set_text(statusTextLayer, STATUS_READY);
+}
+
 
 void cancel_reset_timer()
 {
-	if (statusResetTimer != APP_TIMER_INVALID_HANDLE)
-	{
-		if (app_timer_cancel_event(appContext, statusResetTimer))
-		{
-			statusResetTimer = APP_TIMER_INVALID_HANDLE;
-		}
-	}
+    if (statusResetTimer != NULL)
+    {
+        app_timer_cancel(statusResetTimer);
+        statusResetTimer = NULL;
+    }
 }
 
 void start_reset_timer()
 {
-	cancel_reset_timer();
-	statusResetTimer = app_timer_send_event(appContext, STATUS_RESET_TIME, STATUS_COOKIE);
+    cancel_reset_timer();
+
+    if (statusResetTimer == NULL)
+    {
+        statusResetTimer = app_timer_register(STATUS_RESET_TIME, handle_timer, NULL);
+    }
 }
 
 static void send_cmd(uint8_t cmd)
 {
-	start_reset_timer();
-		
-	Tuplet value = TupletInteger(CMD_KEY, cmd);
-	
-	DictionaryIterator *iter;
-	app_message_out_get(&iter);
-	
-	if (iter == NULL)
-		return;
-	
-	dict_write_tuplet(iter, &value);
-	dict_write_end(iter);
-	
-	app_message_out_send();
-	app_message_out_release();
+    start_reset_timer();
+
+    Tuplet value = TupletInteger(CMD_KEY, cmd);
+
+    DictionaryIterator *iter = NULL;
+    app_message_outbox_begin(&iter);
+    if (iter == NULL)
+        return;
+
+    dict_write_tuplet(iter, &value);
+    dict_write_end(iter);
+
+    app_message_outbox_send();
+    //app_message_out_release();
 }
 
-void handle_timer(AppContextRef app_ctx, AppTimerHandle handle, uint32_t cookie)
-{
-    (void) app_ctx;
-    (void) handle;
-    
-	text_layer_set_text(&statusTextLayer, STATUS_READY);
-}
 
 // Standard app initialisation
 
-void click_config_provider(ClickConfig **config, void *context)
-{
-  config[BUTTON_ID_DOWN]->click.handler = (ClickHandler) down_single_click_handler;
-  config[BUTTON_ID_UP]->click.handler = (ClickHandler) up_single_click_handler;
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_single_click_handler);
 }
 
-void window_load(Window *window)
+
+static void window_load(Window *window)
 {
-	// Initialize the action bar:
-	action_bar_layer_init(&actionBar);
-	// Associate the action bar with the window:
-	action_bar_layer_add_to_window(&actionBar, window);
-	// Set the click config provider:
-	action_bar_layer_set_click_config_provider(&actionBar,
-											   click_config_provider);
-	// Set the icons:
-	heap_bitmap_init( &bitmapRing, RESOURCE_ID_IMAGE_RING_ICON );
-	heap_bitmap_init( &bitmapSilence, RESOURCE_ID_IMAGE_SILENCE_ICON );
-	
-	action_bar_layer_set_icon(&actionBar, BUTTON_ID_UP, &bitmapRing.bmp);
-	action_bar_layer_set_icon(&actionBar, BUTTON_ID_DOWN, &bitmapSilence.bmp);
+    Layer *window_layer = window_get_root_layer(window);
+    GRect window_layer_frame = layer_get_frame(window_layer);
+
+    int16_t aThird = window_layer_frame.size.h / 3;
+
+    GRect textFrame;
+    textFrame.origin.x = 0;
+    textFrame.origin.y = 0;
+    textFrame.size.w = window_layer_frame.size.w;
+    textFrame.size.h = aThird;
+
+    textFrame.origin.y = aThird;
+    statusTextLayer = text_layer_create( textFrame );
+    text_layer_set_text(statusTextLayer, STATUS_READY);
+    text_layer_set_text_alignment(statusTextLayer, GTextAlignmentLeft);
+    text_layer_set_font(statusTextLayer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    layer_add_child(window_layer, text_layer_get_layer(statusTextLayer));
+
+
+    // Initialize the action bar:
+    actionBar = action_bar_layer_create();
+    // Associate the action bar with the window:
+    action_bar_layer_add_to_window(actionBar, window);
+    // Set the click config provider:
+    action_bar_layer_set_click_config_provider(actionBar, click_config_provider);
+
+    // Set the icons:
+    bitmapRing = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_RING_ICON);
+    bitmapSilence = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SILENCE_ICON);
+
+    action_bar_layer_set_icon(actionBar, BUTTON_ID_UP, bitmapRing);
+    action_bar_layer_set_icon(actionBar, BUTTON_ID_DOWN, bitmapSilence);
+
+    register_callbacks();
+
 }
 
-void window_unload(Window *window)
+static void window_unload(Window *window)
 {
-	heap_bitmap_deinit( &bitmapRing );
-	heap_bitmap_deinit( &bitmapSilence );
-}
+    cancel_reset_timer();
 
-void handle_init(AppContextRef ctx)
-{
-  (void)ctx;
-
-	appContext = ctx;
-	resource_init_current_app(&APP_RESOURCES);
-	
-	window_init(&window, WINDOW_TITLE);
-	window_stack_push(&window, true /* Animated */);
-	
-	int16_t aThird = window.layer.frame.size.h / 3;	
-	
-	GRect textFrame;
-	textFrame.origin.x = 0;
-	textFrame.origin.y = 0;
-	textFrame.size.w = window.layer.frame.size.w;
-	textFrame.size.h = aThird;
-	
-	textFrame.origin.y = aThird;
-	text_layer_init(&statusTextLayer, textFrame);
-	text_layer_set_text(&statusTextLayer, STATUS_READY);
-	text_layer_set_text_alignment(&statusTextLayer, GTextAlignmentLeft);
-	text_layer_set_font(&statusTextLayer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-	layer_add_child(&window.layer, &statusTextLayer.layer);
-	
-	register_callbacks();
-	
-	WindowHandlers handlers = {
-		.load = window_load,
-		.unload = window_unload
-	};
-	
-	window_set_window_handlers(&window, handlers);
-}
-
-void handle_deinit(AppContextRef app_ctx)
-{
-	cancel_reset_timer();
-	appContext = NULL;
-}
-
-void pbl_main(void *params)
-{
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-	.deinit_handler = &handle_deinit,
-	.timer_handler = &handle_timer,
-	.messaging_info = {
-      .buffer_sizes = {
-        .inbound = 256,
-        .outbound = 256,
-      }
+    if (actionBar)
+    {
+        action_bar_layer_destroy(actionBar);
+        actionBar = NULL;
     }
-  };
-  app_event_loop(params, &handlers);
+
+    if (bitmapRing)
+    {
+        gbitmap_destroy( bitmapRing );
+        bitmapRing = NULL;
+    }
+    if (bitmapSilence)
+    {
+        gbitmap_destroy( bitmapSilence );
+        bitmapSilence = NULL;
+    }
+
+    if (statusTextLayer)
+    {
+        text_layer_destroy(statusTextLayer);
+        statusTextLayer = NULL;
+    }
 }
+
+
+void init(void)
+{
+    window = window_create();
+
+    window_set_window_handlers(window, (WindowHandlers) {
+            .load = window_load,
+            .unload = window_unload,
+    });
+
+    window_stack_push(window, true /* Animated */);
+
+    const int inbound_size = 256;
+    const int outbound_size = 256;
+    app_message_open(inbound_size, outbound_size);
+}
+
+void deinit(void)
+{
+    window_destroy(window);
+    window = NULL;
+}
+
+int main(void)
+{
+    init();
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+    app_event_loop();
+
+    deinit();
+
+    return 0;
+}
+
